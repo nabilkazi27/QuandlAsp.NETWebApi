8bc18b1998ec720e53aa791e96008ecd3fd1edb8
nabilkazi27 <61580197+nabilkazi27@users.noreply.github.com>
2021-08-20 15:56:29 +0530

    Necessary changes

--- TaskApi/loadData.py
+++ TaskApi/loadData.py
@@ -1,19 +1,23 @@
-import re
 import pandas as p
 import quandl as q
 import numpy as n
 import requests as r
 from datetime import date
 import json
+from urllib3.exceptions import InsecureRequestWarning
+
+# Suppress only the single warning from urllib3 needed.
+r.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 today = date.today()
 todayDate = today.strftime("%Y-%m-%d")
 q.ApiConfig.api_key = "WfVLq3diGsH4L9ETp9RV"
-myData = q.get("BSE/BOM500325", start_date="1992-01-01", end_date=todayDate)
+myData = q.get("BSE/BOM500325", start_date="2015-01-01", end_date=todayDate)
 myData = myData.fillna(0)
 myData = myData.reset_index()
 myData = n.array(myData)
 dataHeader = ['stockDate','stockOpen','stockHigh','stockLow','stockClose','stockWAP','stockNoOfShares','stockNoOfTrades','stockTotalTurnover','stockDeliverableQuantity','stockDeliverableQuantityToTradedQuantityPercent','stockSpreadHL','stockSpreadCO']
+
 def changeData(data):
     transformData = {}
     for index in range(len(data)):
@@ -30,47 +34,11 @@ def changeData(data):
 
 myData = list(map(changeData,myData))
 
-
-url = "http://localhost:5000/Stock"
+url = "http://localhost:5000/api/Stock"
 
 headers = {
   'Content-Type': 'application/json'
 }
-
-'''
-#For 1000 entries per post request
-totalDataLoop = int(n.ceil(len(myData)/1000))
-
-dataLoopCounter = 1
-startRange = 0
-endRange = -1
-responseCount = 0
-
-while(dataLoopCounter<=totalDataLoop):
-    startRange = endRange+1
-
-    if(dataLoopCounter==totalDataLoop):
-        endRange = len(myData)
-    else:
-        endRange = dataLoopCounter * 1000
-
-    dataRange = range(startRange,endRange)
-    data = [myData[i] for i in dataRange]
-
-    payload = json.dumps(data)
-
-    response = r.request("POST", url, headers=headers, data=payload,verify=False)
-
-    if(response):
-        responseCount+=1
-
-    dataLoopCounter+=1
-
-
-if(responseCount==totalDataLoop):
-    print("Data Loaded Successfully")
-
-'''
 
 #For all entry in one post request
 payload = json.dumps(myData)
@@ -79,3 +47,37 @@ response = r.request("POST", url, headers=headers, data=payload,verify=False)
 
 if(response):
     print("Data Loaded Successfully")
+else:
+    print("Data Load Failed")     
+
+#For 1000 entries per post request
+# totalDataLoop = int(n.ceil(len(myData)/1000))
+
+# dataLoopCounter = 1
+# startRange = 0
+# endRange = -1
+# responseCount = 0
+
+# while(dataLoopCounter<=totalDataLoop):
+#     startRange = endRange+1
+
+#     if(dataLoopCounter==totalDataLoop):
+#         endRange = len(myData)
+#     else:
+#         endRange = dataLoopCounter * 1000
+
+#     dataRange = range(startRange,endRange)
+#     data = [myData[i] for i in dataRange]
+
+#     payload = json.dumps(data)
+
+#     response = r.request("POST", url, headers=headers, data=payload,verify=False)
+
+#     if(response):
+#         responseCount+=1
+
+#     dataLoopCounter+=1
+
+
+# if(responseCount==totalDataLoop):
+#     print("Data Loaded Successfully")
